e37cebb4d038939f511909c146ec8635898a01ea
Ahmed Mahdy <am.elnaddar@gmail.com>
2025-07-04 22:29:52 +0300

    feat: implement CRUD trait with error handling and success responses

---
+++ src/traits/crud.rs
@@ -0,0 +1,19 @@
+use std::collections::HashMap;
+
+pub struct Error {
+    pub msg: String,
+}
+
+pub struct Success {
+    pub msg: String,
+}
+
+pub trait Crud<K, V> {
+    fn init_storage() -> Result<Success, Error>;
+    fn write_all(v: HashMap<K, V>) -> Result<Success, Error>;
+    fn get_all() -> Result<HashMap<K, V>, Error>;
+    fn get(key: &K) -> Result<V, Error>;
+    fn create(t: V) -> Result<Success, Error>;
+    fn update(key: &K, v: V) -> Result<Success, Error>;
+    fn delete(key: &K) -> Result<Success, Error>;
+}
